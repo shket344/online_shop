49d86d17094db95c5cfb435abb802f985cbc1790
shket344 <kalimador@tut.by>
2023-11-16 19:34:47 +0300

    (OS-1): create Product model

---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Product < ApplicationRecord
+  validates :title, :price, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+end
