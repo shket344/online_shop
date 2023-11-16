43485d7d261771c313e12064258e6a78d64a5c06
shket344 <kalimador@tut.by>
2023-11-16 19:39:19 +0300

    (OS-1): annotate Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  image_url   :string
+#  price       :decimal(8, 2)    not null
+#  title       :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Product < ApplicationRecord
   validates :title, :price, presence: true
   validates :title, uniqueness: true
