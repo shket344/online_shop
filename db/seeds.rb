6777f123c3a49304a3a5f362ce27965fe06d2713
shket344 <kalimador@tut.by>
2023-11-22 14:02:33 +0300

    (OS-2): update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-5.times do |iterator|
-  price = "0.#{iterator}"
-  title = Faker::Beer.brand
-  Product.create(title: title, price: price)
+5.times do
+  FactoryBot.create(:product)
 end
