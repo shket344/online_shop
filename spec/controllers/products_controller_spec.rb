4acbb4bd40b17c5f55b0d666c135f813db837c98
shket344 <kalimador@tut.by>
2023-11-27 12:21:34 +0300

    (OS-2): update ProductsController spec

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -4,10 +4,11 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
   let(:product) { create(:product) }
+  let(:category) { product.category }
 
   describe '#index' do
     it 'renders index template' do
-      get :index
+      get :index, params: { category_id: category.id }
 
       expect(response).to render_template('index')
     end
@@ -15,7 +16,7 @@ RSpec.describe ProductsController, type: :controller do
 
   describe '#show' do
     it 'returns product page' do
-      get :show, params: { id: product.id }, format: :html
+      get :show, params: { id: product.id, category_id: category.id }, format: :html
 
       expect(assigns(:product)).to eq(product)
     end
@@ -23,7 +24,7 @@ RSpec.describe ProductsController, type: :controller do
 
   describe '#new' do
     it 'assigns a new product as @product' do
-      get :new
+      get :new, params: { category_id: category.id }, format: :html
       expect(assigns(:product)).to be_a_new(Product)
     end
   end
@@ -33,15 +34,16 @@ RSpec.describe ProductsController, type: :controller do
 
     context 'with valid attributes' do
       it 'creates new product' do
-        post :create, params: { product: product_params }
+        post :create, params: { product: product_params, category_id: category.id }
 
-        expect(response).to have_http_status(:created)
+        product = Product.find_by(title: product_params[:title])
+        expect(response).to redirect_to category_product_path(category_id: category.id, id: product.id)
       end
     end
 
     context 'with invalid attributes' do
       it 'does not create new product' do
-        post :create, params: { product: product_params.except(:title) }
+        post :create, params: { product: product_params.except(:title), category_id: category.id }
 
         expect(response).to render_template('new')
       end
@@ -50,7 +52,7 @@ RSpec.describe ProductsController, type: :controller do
 
   describe '#edit' do
     it 'assigns the requested product as @product' do
-      get :edit, params: { id: product.id }
+      get :edit, params: { id: product.id, category_id: category.id }
       expect(assigns(:product)).to eq(product)
     end
   end
@@ -58,15 +60,15 @@ RSpec.describe ProductsController, type: :controller do
   describe '#update' do
     context 'with valid attributes' do
       it 'updates product' do
-        put :update, params: { id: product.id, product: { title: 'test' } }
+        put :update, params: { id: product.id, product: { title: 'test' }, category_id: category.id }
 
-        expect(response).to have_http_status(:ok)
+        expect(response).to redirect_to category_product_path(category_id: category.id, id: product.id)
       end
     end
 
     context 'with invalid attributes' do
       it 'does not update product' do
-        put :update, params: { id: product.id, product: { title: nil } }
+        put :update, params: { id: product.id, product: { title: nil }, category_id: category.id }
 
         expect(response).to render_template('edit')
       end
@@ -75,7 +77,7 @@ RSpec.describe ProductsController, type: :controller do
 
   describe '#destroy' do
     it 'destroys product' do
-      delete :destroy, params: { id: product.id }
+      delete :destroy, params: { id: product.id, category_id: category.id }
 
       expect(response).to have_http_status(:see_other)
     end
