6bf1e25398361e9c1b76c7ee5565376e8f3a7c45
shket344 <kalimador@tut.by>
2023-12-15 14:18:27 +0300

    (OS-4): create refference between user and role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: roles
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Role < ApplicationRecord
+  has_many :users
   validates_presence_of :title
+  validates :title, uniqueness: true
 end
