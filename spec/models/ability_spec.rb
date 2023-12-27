dd21a251a5c3dada65362284d59b3757531c6bae
shket344 <kalimador@tut.by>
2023-12-27 13:47:31 +0300

    (OS-4): minor changes

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -6,6 +6,7 @@ require 'rails_helper'
 describe 'User' do
   describe 'abilities' do
     subject(:ability) { Ability.new(user) }
+    let(:user) { create(:user, role: role) }
 
     context 'when there is no user' do
       let(:user) { nil }
@@ -15,14 +16,12 @@ describe 'User' do
 
     context 'when is admin' do
       let(:role) { create(:role, title: 'admin') }
-      let(:user) { create(:user, role: role) }
 
       it { is_expected.to be_able_to(:manage, :all) }
     end
 
     context 'when is user' do
       let(:role) { create(:role, title: 'user') }
-      let(:user) { create(:user, role: role) }
 
       it { is_expected.to be_able_to(:read, :all) }
       it { is_expected.to be_able_to(:manage, User) }
