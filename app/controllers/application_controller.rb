d2db14eb4f831c3fce0af93db699f5c05597c5a0
shket344 <kalimador@tut.by>
2024-01-05 17:09:57 +0300

    (OS-5): redirect if no access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def access_denied(exception)
+    redirect_to root_path, alert: exception.message
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name surname])
