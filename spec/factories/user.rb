5a57d2071deded45d77787c2a9a8df218fb8d870
shket344 <kalimador@tut.by>
2024-01-08 15:41:11 +0300

    (OS-6): improve ability spec

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -13,5 +13,17 @@ FactoryBot.define do
         FactoryBot.create(:cart, user: user)
       end
     end
+
+    trait :owner do
+      role { FactoryBot.create(:role, title: 'owner') }
+    end
+
+    trait :admin do
+      role { FactoryBot.create(:role, title: 'admin') }
+    end
+
+    trait :simple_user do
+      role { FactoryBot.create(:role, title: 'user') }
+    end
   end
 end
